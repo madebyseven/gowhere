790f0f82663ac6a6b88779d158176d54a86ae6c2
colt <colt@gmail.com>
2024-09-11 21:30:17 +0800

    fix all classname attribute issue/error

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -12,23 +12,23 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <div class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse grid-cols-1  md:gap-8 gap-16">
-        <div class="xl:pt-8 w-full relative pb-12 lg:pb-0">
-          <div class="relative flex justify-center items-center">
-            <div class="absolute">
-              <div class="">
-                <h1 class="my-2 text-gray-800 font-bold text-3xl">
+      <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse grid-cols-1  md:gap-8 gap-16">
+        <div className="xl:pt-8 w-full relative pb-12 lg:pb-0">
+          <div className="relative flex justify-center items-center">
+            <div className="absolute">
+              <div className="">
+                <h1 className="my-2 text-gray-800 font-bold text-3xl">
                   Oops! Page {error.statusText || error.message}
                 </h1>
-                {/* <h2 class="my-2 text-gray-800 font-bold text-2xl">
+                {/* <h2 className="my-2 text-gray-800 font-bold text-2xl">
                   Looks like you've found the doorway to the great nothing
                 </h2> */}
-                <p class="my-2 text-gray-800">
+                <p className="my-2 text-gray-800">
                   Sorry about that! Please visit our hompage to get where you
                   need to go.
                 </p>
                 <button
-                  class="bg-indigo-500 text-white font-medium py-2 px-4 rounded transition-all hover:bg-indigo-600 active:scale-95"
+                  className="bg-indigo-500 text-white font-medium py-2 px-4 rounded transition-all hover:bg-indigo-600 active:scale-95"
                   onClick={navigateHome}
                 >
                   Take me there!
